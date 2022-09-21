9bd30a5bae0ac369db262201f4ed1707410f7dae
Sepand <90920994+sepandj@users.noreply.github.com>
2022-09-20 23:27:39 -0400

    Add files via upload

---
+++ DataSetGen.java
@@ -0,0 +1,35 @@
+
+public class DataSetGen<T extends Measurable> {
+	
+	private double sum;
+	private T maximum;
+	private int count;
+	
+	public DataSetGen() {
+		sum = 0;
+		maximum = null;
+		count = 0;
+	}
+	
+	
+	public void add(T x) {
+		sum = sum + x.getMeasure();
+		if (count==0 || maximum.getMeasure()<x.getMeasure())
+			maximum = x;
+		count++;
+	}
+	
+	public double getAverage() {
+		if(count==0)
+			return 0;
+		else
+			return sum/count;
+	}
+	
+	public T getMaximum() {
+		return maximum;
+	}
+	
+	
+
+}
